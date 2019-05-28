f243d1be87001656237d073d7fb215867bb03f8e
gleekzorp <gleek.dan@gmail.com>
2019-05-27 18:24:44 -0600

    Finished part 2

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import JournalList from './journals/journal_list/journal_list';
+import JournalList from './journals/journal_list';
 
 export default class App extends Component {
   render() {
@@ -7,7 +7,6 @@ export default class App extends Component {
       <div className='app'>
         <h1>React, Props, and State Deep Dive</h1>
         <JournalList heading="List 1"/>
-        <JournalList heading="List 2"/>
       </div>
     );
   }
